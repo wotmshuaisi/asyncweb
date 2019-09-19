369a9d2d103d9dd169ff225464bd4a72350e6c99
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-09-19 10:44:48 +0100

    setcookie delcookie in AsyncResponse supported

--- response.py
+++ response.py
@@ -5,35 +5,52 @@ class AsyncResponse:
     StatusCode: int
     Headers: dict
     Body: str
+    __cookie__: dict
 
     def __init__(self, **kwargs):
         self.StatusCode = kwargs.get("status_code") or 204
         self.Headers = kwargs.get("headers") or {}
         self.Body = kwargs.get("body") or ""
+        self.__cookie__ = {}
 
     def set_header(self, k, v):
         self.Headers[k] = v
 
     def JSON(self, data):
         self.StatusCode = 200
-        self.set_header("Content-type", "application/json")
+        self.set_header("Content-Type", "application/json")
         self.Body = json.dumps(data)
 
     def Text(self, data: str):
         self.StatusCode = 200
-        self.set_header("Content-type", "text/plain")
+        self.set_header("Content-Type", "text/plain")
         self.Body = data
 
     def Html(self, data: str):
         self.StatusCode = 200
-        self.set_header("Content-type", "text/html")
+        self.set_header("Content-Type", "text/html")
         self.Body = data
+
+    def Redirect(self, path: str):
+        self.StatusCode = 302
+        self.set_header("Location", path)
+
+    def SetCookie(self, key: str, val: str):
+        self.__cookie__[key] = val
+
+    def DelCookie(self, key: str,):
+        del self.__cookie__[key]
 
     @property
     def __headers_serialized__(self, ):
         tmp = ""
         for k, v in self.Headers.items():
             tmp += '{}:{}\r\n'.format(k, v)
+        if len(self.__dict__) > 0:
+            cookies = ''
+            for k, v in self.__cookie__.items():
+                cookies += k+"="+v+";"
+            tmp += 'Set-Cookie:{}'.format(cookies)
         return tmp
 
     def __toByes__(self,):
